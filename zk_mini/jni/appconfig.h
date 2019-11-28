62ee9ac98e65fa351201b250c02c442f311afbc6
koda.xu <koda.xu@mstarsemi.com>
2019-11-28 19:49:28 +0800

    [zk_player][zk_mini] add zk_mini.

---
+++ zk_mini/jni/appconfig.h
@@ -0,0 +1,18 @@
+/*
+ * appconfig.h
+ *
+ *  Created on: 2019年10月22日
+ *      Author: koda.xu
+ */
+
+#ifndef JNI_APPCONFIG_H_
+#define JNI_APPCONFIG_H_
+
+#define USE_PANEL_1024_600		1
+#define USE_AMIC				1
+
+#define CSPOTTER_PATH_PREFIX        "/customer/res/CSpotter"
+#define CSPOTTER_LIB_PATH           "/customer/res/CSpotter/lib"
+#define CSPOTTER_DATA_PATH          "/customer/res/CSpotter/data"
+
+#endif /* JNI_APPCONFIG_H_ */
